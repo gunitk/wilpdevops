9936e547da592593c58bfff729b61b03725800a9
Gunit Wilp <gunitwilp@Vinays-MacBook-Air.local>
2024-09-07 18:18:42 +0530

    Edits done by collaborator user

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ pulic class HelloWorld{
 public static void main (String args[])
 {
 	System.out.println("Hello World");
-	System.out.println("This   second command is added by gunitkwilp who is a collabortor in this project"
+	System.out.println("This second command is added by gunitkwilp who is a collabortor in this project"
 }
 
 }
