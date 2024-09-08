04a45cb4fef1e2f16baa17768234be086143cef8
Gunit Kumar <gunitkumar@Vinays-MacBook-Air.local>
2024-09-08 16:01:31 +0530

    Made changes to System.out.println for HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ pulic class HelloWorld{
 
 public static void main (String args[])
 {
-	System.out.println("Hello World");
+	System.out.println("Hello World and this word is added by the owner");
 	System.out.println("This  command is added by gunitkwilp who is a collabortor in this project"
 }
 
